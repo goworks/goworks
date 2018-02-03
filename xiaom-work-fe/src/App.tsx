68f9f37a51908aa5030c18a9357d18b047d1def0
佳九 <frank.lsl@alibaba-inc.com>
2018-02-03 17:50:36 +0800

    material-ui + typescript

--- xiaom-work-fe/src/App.tsx
+++ xiaom-work-fe/src/App.tsx
@@ -1,22 +1,32 @@
 import * as React from 'react';
+import Button from 'material-ui/Button';
+import withStyles, { WithStyles, StyleRulesCallback } from 'material-ui/styles/withStyles';
+import Reboot from 'material-ui/Reboot';
 import './App.css';
 
-const logo = require('./logo.svg');
+const styles: StyleRulesCallback<'root'> = (theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+});
 
-class App extends React.Component {
+type State = {
+  open: boolean;
+};
+
+class App extends React.Component<WithStyles<'root'>, State> {
   render() {
+    const {classes} = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className={classes.root}>
+        <Reboot/>
+        <Button raised={true} color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)<{}>(App);
